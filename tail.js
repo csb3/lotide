a18fe124360698a1e1f617183f7d8d220f5f27e6
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-04 23:18:57 +0000

    created function to return tail of an array

---
+++ tail.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  const tailValue = array.slice(1);
+  return tailValue;
+};
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+// Test Case: Array with one element
+console.log(tail([1]));
+
+// Test Case: Empty Array
+
+console.log(tail([]));
