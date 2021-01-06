fe33cb3d93b5575660d994f4919df3ed898855c8
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-06 04:07:51 +0000

    added flatten function

--- flatten.js
+++ flatten.js
@@ -18,6 +18,19 @@ const assertArraysEqual = function(testArray1, testArray2) {
     console.log(`🛑🛑🛑 Assertion Failed: ${testArray1} !== ${testArray2}`);
   }
 };
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 3, 3], [1, 2, 3]);
-assertArraysEqual([1, 3, 3], [1, 2, "3"]);
+
+const flatten = function(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        newArray.push(array[i][j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
