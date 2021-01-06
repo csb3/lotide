3d60f64e5ed75db655143e90f8023019e64e5fc8
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-06 01:10:04 +0000

    created assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(testArray1, testArray2) {
+  if (eqArrays(testArray1, testArray2)) {
+    console.log(`✅✅✅ Assertion Passed: ${testArray1} === ${testArray2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${testArray1} !== ${testArray2}`);
+  }
+};
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 3, 3], [1, 2, 3]);
+assertArraysEqual([1, 3, 3], [1, 2, "3"]);
