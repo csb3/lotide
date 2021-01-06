8ffdb6006ef31bdfe467836c91dc5e44401296f2
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-06 01:03:02 +0000

    fixed semicolon and trailing space

--- eqArrays.js
+++ eqArrays.js
@@ -10,7 +10,7 @@ const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
-        return false
+        return false;
       }
     }
   } else {
@@ -22,6 +22,6 @@ const eqArrays = function(array1, array2) {
 console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); 
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
