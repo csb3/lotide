b9102f87f50e79a9853581e2f5c1b3d607ccec2a
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-12 07:14:04 +0000

    updated test case

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -2,7 +2,6 @@ const assertEqual = require('../assertEqual');
 
 assertEqual("LighthouseLabs", "Bootcamp");
 assertEqual(1, 1);
-assertEqual("Cass", "Casscasscass");
-assertEqual("Cass", "Cass");
+assertEqual("Lighthouse", "Lighthouse");
 assertEqual(123, 123);
 assertEqual(123, 124);
